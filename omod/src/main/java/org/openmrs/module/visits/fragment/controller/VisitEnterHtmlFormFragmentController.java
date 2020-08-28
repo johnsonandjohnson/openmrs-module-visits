29a5da918fe6566ed107e1446d4200167949b289
Arkadiusz Lalo <alalo@soldevelo.com>
2020-08-28 11:24:18 +0200

    CFLM-1360: Fixed issue with the wrong dependency

--- omod/src/main/java/org/openmrs/module/visits/fragment/controller/VisitEnterHtmlFormFragmentController.java
+++ omod/src/main/java/org/openmrs/module/visits/fragment/controller/VisitEnterHtmlFormFragmentController.java
@@ -33,7 +33,7 @@ import org.openmrs.ui.framework.annotation.SpringBean;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Date;
