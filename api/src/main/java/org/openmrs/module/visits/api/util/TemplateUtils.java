d8d7a5e5a72e4e31d866bed8c37d25f0694067f7
Piotr Wargulak <pwargulak@soldevelo.com>
2021-03-16 14:44:05 +0000

    AGRE-1477: Fixed ReDoS From Regex Injection\Path 1
    * AGRE-1477: Fixed ReDoS From Regex Injection\Path 1
    
    Changed the template parameter replacement from Regex to regular string substring replacement.
    The former Regex didn't utilize any particular feature of regular expressions.
    
    Approved-by: Kamil Madej

--- api/src/main/java/org/openmrs/module/visits/api/util/TemplateUtils.java
+++ api/src/main/java/org/openmrs/module/visits/api/util/TemplateUtils.java
@@ -2,9 +2,14 @@ package org.openmrs.module.visits.api.util;
 
 import java.util.Map;
 
+import static org.springframework.util.ObjectUtils.nullSafeToString;
+
 public final class TemplateUtils {
-    private static final String START_PARENTHESIS_REGEX = "\\{\\{";
-    private static final String END_PARENTHESIS_REGEX = "\\}\\}";
+    private static final String START_PARENTHESIS = "{{";
+    private static final String END_PARENTHESIS = "}}";
+
+    private TemplateUtils() {
+    }
 
     public static String fillTemplate(String template, Map<String, String> params) {
         String result = template;
@@ -14,10 +19,7 @@ public final class TemplateUtils {
         return result;
     }
 
-    public static String fillTemplate(String template, String param, String value) {
-        return template.replaceAll(START_PARENTHESIS_REGEX + param + END_PARENTHESIS_REGEX, value);
-    }
-
-    private TemplateUtils() {
+    static String fillTemplate(String template, String param, String value) {
+        return template.replace(START_PARENTHESIS + param + END_PARENTHESIS, nullSafeToString(value));
     }
 }
