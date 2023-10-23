2d4d5114ddf18b744a84b17ae193ec168eb2c531
druchniewicz <druchniewicz@soldevelo.com>
2023-10-23 13:57:04 +0200

    Increased delay time when visit is updated

--- owa/app/js/shared/global-constants.ts
+++ owa/app/js/shared/global-constants.ts
@@ -1,3 +1,3 @@
 /** Delay in MS to apply to any refresh made after creation or an update of a Visit. It's workaround for asynchronous
  *  creation of future Visits by BE that FE has no way to know about. */
-export const VISIT_SAVE_DELAY_MS = 200;
+export const VISIT_SAVE_DELAY_MS = 500;
